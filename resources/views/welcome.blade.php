2233e88a7e96a4acd3baff95068e9d3868bc6b7c
Tagliapietra96 <tagliatek96@gmail.com>
2023-03-01 17:18:08 +0100

    create welcome banner

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="home-jumbotron">
+<div class="home-jumbotron pt-5">
+
+    <div class="container bg-light rounded-4 p-5 presentation-banner">
+        <h1 class="text-center">Welcome on Deliveroo Restaurateur's page</h1>
+    </div>
 
 </div>
 @endsection
