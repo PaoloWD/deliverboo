40add172c375723316cfb7411e7c6a34eae9c932
Tagliapietra96 <tagliatek96@gmail.com>
2023-03-01 17:24:49 +0100

    change banner color

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 
 <div class="home-jumbotron pt-5">
 
-    <div class="container bg-light rounded-4 p-5 presentation-banner">
+    <div class="container  rounded-4 p-5 presentation-banner">
         <h1 class="text-center">Welcome on Deliveroo Restaurateur's page</h1>
     </div>
 
