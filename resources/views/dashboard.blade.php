301d08a72eacf6c0fbaafabf0c7dd963c57021ac
VenturelliGiorgia <venturelli.giorgia.03@gmail.com>
2023-03-01 17:03:14 +0100

    create dashboard branche

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,22 +5,22 @@
     <h2 class="fs-4 text-secondary my-4">
         {{ __('Dashboard') }}
     </h2>
-    <div class="row justify-content-center">
-        <div class="col">
-            <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
+            <div>
+                <div class="card-header">{{ __('Welcome in your Dashboard') }}</div>
 
-                <div class="card-body">
+                <div>
                     @if (session('status'))
                     <div class="alert alert-success" role="alert">
                         {{ session('status') }}
                     </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <div class="text-center">
+                    {{ __('Create your restaurant profile!') }}
+                        <h4>Add your restaurant </h4>
+                        <button class="btn btn-primary px-3"> + </button>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
 </div>
 @endsection
