fe1ba501ca4fbe8d31b136189fc6d63ae34ecc08
VenturelliGiorgia <venturelli.giorgia.03@gmail.com>
2023-03-03 11:55:03 +0100

    edit button + responsive card

--- resources/views/dishes/show.blade.php
+++ resources/views/dishes/show.blade.php
@@ -8,37 +8,38 @@
             <div class="row h-100 overflow-auto align-items-center justify-content-center">
                 <h1 class="custom-color text-center col-12">Your dishes!</h1>
                 @foreach ($dishes as $dish)
-                <div class="col-3">
+                <div class="col-12 col-md-3 col-lg-4">
                     <div class="card shadow dashboard mt-4 p-4">
                         <div class="card-title text-center">
                             <h2> {{ $dish->name }} </h2>
                         </div>
                         <div class="card-img-top text-center pb-3">
-                            <img class="img-fluid" src="" alt="">
+                            <img class="img-fluid" src="{{ asset('storage/' . $dish->image) }}" alt="">
                         </div>
                         <div class="card-text">
                             <p>Description: {{ $dish->description }} </p>
                             <p>Ingredients: {{ $dish->ingredients }} </p>
                             <p>Price: {{ $dish->price }}€</p>
                         </div>
-                        <div class="text-center mt-3">
-                            <a href="{{ route('dashboard') }}">
-                                <button class="btn btn-success btn-custom"> Back to Dashboard </button>
+                        <div class="d-flex gap-2 d-md-inline-block d-lg-flex justify-content-center">
+                            <a class="link-show" href="{{ route('dashboard') }}">
+                                <button class="btn btn-success btn-custom">
+                                    <i class="fa-solid fa-arrow-left"></i>
+                                </button>
                             </a>
-                        </div>
-                        <div class="text-center mt-3">
-                            <a href={{ route('dishes.edit', $dish->id) }}>
-                                <button class="btn btn-success btn-custom"> Modifica </button>
+                            <a class="link-show" href={{ route('dishes.edit', $dish->id) }}>
+                                <button class="btn btn-custom"> 
+                                    <i class="fa-solid fa-pen"></i>    
+                                </button>
                             </a>
-                        </div>
                         <form action="{{ route('dishes.destroy', $dish->id) }}" method="POST"
                             class="delete-form d-inline-block">
                             @csrf()
                             @method('delete')
-    
-                            <button class="btn btn-danger">
-                                X
-                            </button>
+                                <button class="btn btn-success btn-custom mt-md-1 mt-lg-0">
+                                    <i class="fa-solid fa-trash"></i>
+                                </button>
+                            </div>
                         </form>
                     </div>
                 </div>
