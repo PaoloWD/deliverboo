f2582a2014d232203b9ca91c6b6da51cb75c95d2
VenturelliGiorgia <venturelli.giorgia.03@gmail.com>
2023-03-06 16:00:12 +0100

    added restaurant image

--- resources/views/restaurants/show.blade.php
+++ resources/views/restaurants/show.blade.php
@@ -10,9 +10,13 @@
                     <div class="card-title text-center">
                         <h2></h2>
                     </div>
-                    <div class="card-img-top text-center">
-                        <img class="img-fluid" src="{{ asset('storage/' . $restaurant->image) }}" alt="" style="height:200px" alt="">
-                    </div>
+                    @if(str_contains($restaurant->image, "https"))
+                        <img class="card-img-top restaurant-img pb-3" src="{{$restaurant->image}}" alt="" style="height: 200px">
+                        @elseif (!$restaurant->image)
+                            <img class="card-img-top restaurant-img pb-3" src="https://www.lerocce.com/wp-content/uploads/2019/01/terrazza_ulivi.jpg" alt="" style="height: 200px">
+                        @else  
+                            <img class="card-img-top restaurant-img pb-3" src="{{ asset('storage/' . $restaurant->image) }}" class="card-img restaurant-img" alt="" style="height: 200px">
+                    @endif
                     <div class="card-text">
                         <p><strong>Nome del Ristorante:</strong> {{$restaurant->name}}</p>
                         <p><strong>Partita iva:</strong> {{$restaurant->vat}}</p>
