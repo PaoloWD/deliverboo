3fe51b23b958e203d7e999f6d6f67d204681b174
Tagliapietra96 <tagliatek96@gmail.com>
2023-03-06 11:04:41 +0100

    fixed respinsivness

--- resources/views/dishes/show.blade.php
+++ resources/views/dishes/show.blade.php
@@ -8,54 +8,54 @@
             <div class="row h-100 overflow-auto justify-content-center">
                 <h1 class="custom-color text-center col-12">Your dishes!</h1>
                 @foreach ($dishes as $dish)
-                <div class="col-12 col-md-6 col-lg-4 pb-3 ">
-                    <div class="py-5 h-100">
-                        <div class="card shadow dashboard mt-4 p-4 h-100 rounded-4">
-                            <div class="card-title title-dishes text-center">
-                                <h2> {{ $dish->name }} </h2>
-                            </div>
-                            <div class="card-img-top text-center pb-3">
-                                @if(str_contains($dish->image, "https"))
-                                    <img class="card-img-top dish-img" src="{{$dish->image}}"
-                                    alt="" style="height:230px">
-                                    @else
-                                    <img class="card-img-top dish-img" src="{{ asset('storage/' . $dish->image) }}"
-                                    alt="" style="height:230px">
-                                @endif
-                            </div>
-                            <div class="card-text flex-grow-1">
-                                <p><strong>Description:</strong> {{ $dish->description }} </p>
-                                <p><strong>Ingredients:</strong> {{ $dish->ingredients }} </p>
-                                <p><strong>Price:</strong> {{ number_format($dish->price, 2, ',', '.') }} €</p>
-                            </div>
-                            <div class="d-flex gap-2  justify-content-center">
-                                <a class="link-show" href="{{ route('dashboard') }}">
-                                    <button class="btn btn-success btn-custom">
-                                        <i class="fa-solid fa-arrow-left"></i>
-                                    </button>
-                                </a>
-                                <a class="link-show" href={{ route('dishes.edit', $dish->id) }}>
-                                    <button class="btn btn-custom"> 
-                                        <i class="fa-solid fa-pen"></i>    
-                                    </button>
-                                </a>
-                            <form action="{{ route('dishes.destroy', $dish->id) }}" method="POST"
-                                class="delete-form d-inline-block">
-                                @csrf()
-                                @method('delete')
-                                    <button class="btn btn-success btn-custom ">
-                                        <i class="fa-solid fa-trash"></i>
-                                    </button>
+                    <div class="col-12 col-md-6 col-lg-4 pb-3 ">
+                        <div class="py-5 h-100">
+                            <div class="card shadow dashboard mt-4 p-4 h-100 rounded-4">
+                                <div class="card-title title-dishes text-center">
+                                    <h2> {{ $dish->name }} </h2>
                                 </div>
-                            </form>
+                                <div class="card-img-top text-center pb-3">
+                                    @if (str_contains($dish->image, 'https'))
+                                        <img class="card-img-top dish-img" src="{{ $dish->image }}" alt=""
+                                            style="height:230px">
+                                    @else
+                                        <img class="card-img-top dish-img" src="{{ asset('storage/' . $dish->image) }}"
+                                            alt="" style="height:230px">
+                                    @endif
+                                </div>
+                                <div class="card-text flex-grow-1">
+                                    <p><strong>Description:</strong> {{ $dish->description }} </p>
+                                    <p><strong>Ingredients:</strong> {{ $dish->ingredients }} </p>
+                                    <p><strong>Price:</strong> {{ number_format($dish->price, 2, ',', '.') }} €</p>
+                                </div>
+                                <div class="d-flex gap-2  justify-content-center">
+                                    <a class="link-show" href="{{ route('dashboard') }}">
+                                        <button class="btn btn-success btn-custom">
+                                            <i class="fa-solid fa-arrow-left"></i>
+                                        </button>
+                                    </a>
+                                    <a class="link-show" href={{ route('dishes.edit', $dish->id) }}>
+                                        <button class="btn btn-custom">
+                                            <i class="fa-solid fa-pen"></i>
+                                        </button>
+                                    </a>
+                                    <form action="{{ route('dishes.destroy', $dish->id) }}" method="POST"
+                                        class="delete-form d-inline-block">
+                                        @csrf()
+                                        @method('delete')
+                                        <button class="btn btn-success btn-custom ">
+                                            <i class="fa-solid fa-trash"></i>
+                                        </button>
+                                    </form>
+                                </div>
+                                
+                            </div>
                         </div>
+
                     </div>
-                    
-                </div>
-                
-            @endforeach
+                @endforeach
             </div>
-            
+
         </div>
 
 
@@ -64,11 +64,11 @@
         const forms = document.querySelectorAll(".delete-form");
         forms.forEach((form) => {
             form.addEventListener("submit", function(e) {
-            e.preventDefault();
-            const conferma = confirm("Sicuro?");
-            if (conferma === true) {
-                form.submit();
-            }
+                e.preventDefault();
+                const conferma = confirm("Sicuro?");
+                if (conferma === true) {
+                    form.submit();
+                }
             })
         })
     </script>
