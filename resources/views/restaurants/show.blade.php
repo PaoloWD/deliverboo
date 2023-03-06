fe6425b4e266af7d41cd8c4b28f3ad88d1f5bfec
VenturelliGiorgia <venturelli.giorgia.03@gmail.com>
2023-03-06 12:27:31 +0100

    change language create + show restaurants

--- resources/views/restaurants/show.blade.php
+++ resources/views/restaurants/show.blade.php
@@ -5,7 +5,7 @@
     <div class="container">
         <div class="pt-5 text-center">
             <div class="card p-5 rounded-5" style="opacity: 0.95">
-                <h1 class="custom-color">This is your restaurant!</h1>
+                <h1 class="custom-color">Questo è il tuo ristorante!</h1>
                 <div class="card show-restaurant mt-4 p-4">
                     <div class="card-title text-center">
                         <h2></h2>
@@ -15,7 +15,7 @@
                     </div>
                     <div class="card-text">
                         <p>Nome del Ristorante: {{$restaurant->name}}</p>
-                        <p>VAT: {{$restaurant->vat}}</p>
+                        <p>Partita iva: {{$restaurant->vat}}</p>
                         <p>Indirizzo: {{$restaurant->address}}</p>
                         <p>Categorie:
                             @foreach ($restaurant->categories as $categories )
@@ -27,7 +27,7 @@
                     </div>
                     <div class="text-center mt-4">
                         <a href="{{ route('dashboard') }}">
-                            <button class="btn btn-success btn-custom"> Back to Dashboard </button>
+                            <button class="btn btn-success btn-custom"> Torna al pannello di controllo </button>
                         </a>
                     </div>
                 </div>
