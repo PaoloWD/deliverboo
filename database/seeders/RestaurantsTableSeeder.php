f55b6ebee0ac7713cbfd7f80a1021238e3321588
Pelliccia96 <fra.pelliccia96@gmail.com>
2023-03-06 11:08:59 +0100

    Fixed Seeder Restaurants image

--- database/seeders/RestaurantsTableSeeder.php
+++ database/seeders/RestaurantsTableSeeder.php
@@ -22,7 +22,7 @@ class RestaurantsTableSeeder extends Seeder
                 $category_id = Category::find(1)->id, // Recupera l'ID della categoria con ID = 1
                 $restaurant = new Restaurant(),
                 $restaurant->name = 'Ristorante da Francesco',
-                $restaurant->image = '',
+                $restaurant->image = 'https://www.mongalletto.it/wp-content/uploads/2016/10/ristorante-terrazza.jpg',
                 $restaurant->vat = '12345678901',
                 $restaurant->address = 'Via Roma, 1',
                 $restaurant->user_id = 1,
@@ -35,7 +35,7 @@ class RestaurantsTableSeeder extends Seeder
                 $category_id = Category::find(3)->id, // Recupera l'ID della categoria con ID = 3
                 $restaurant = new Restaurant(),
                 $restaurant->name = 'Ristorante da Paolo',
-                $restaurant->image = '',
+                $restaurant->image = 'https://www.lerocce.com/wp-content/uploads/2019/01/terrazza_ulivi.jpg',
                 $restaurant->vat = '56345348901',
                 $restaurant->address = 'Via Roma, 4',
                 $restaurant->user_id = 2,
@@ -47,7 +47,7 @@ class RestaurantsTableSeeder extends Seeder
                 $category_id = Category::find(7)->id, // Recupera l'ID della categoria con ID = 7
                 $restaurant = new Restaurant(),
                 $restaurant->name = 'Ristorante da Matteo',
-                $restaurant->image = '',
+                $restaurant->image = 'https://media-magazine.trivago.com/agriturismo-con-spa-umbria-borgomandoleto-esterno.jpg',
                 $restaurant->vat = '99996348901',
                 $restaurant->address = 'Via Roma, 5',
                 $restaurant->user_id = 3,
@@ -58,7 +58,7 @@ class RestaurantsTableSeeder extends Seeder
                 $category_id = Category::find(10)->id, // Recupera l'ID della categoria con ID = 10
                 $restaurant = new Restaurant(),
                 $restaurant->name = 'Ristorante da Luiggina',
-                $restaurant->image = '',
+                $restaurant->image = 'https://agriturismocaiferri.it/wp-content/uploads/2022/06/IMG-20220627-WA0004.jpg',
                 $restaurant->vat = '66445348901',
                 $restaurant->address = 'Via Roma, 6',
                 $restaurant->user_id = 4,
@@ -69,7 +69,7 @@ class RestaurantsTableSeeder extends Seeder
                 $category_id = Category::find(3)->id, // Recupera l'ID della categoria con ID = 3
                 $restaurant = new Restaurant(),
                 $restaurant->name = 'Ristorante da Giorgia',
-                $restaurant->image = '',
+                $restaurant->image = 'https://www.ristorantemovida.it/wp-content/uploads/2020/10/ristorante_movida_bg-home_2.jpg',
                 $restaurant->vat = '56374659801',
                 $restaurant->address = 'Via Roma, 7',
                 $restaurant->user_id = 5,
