c4c871538bdfde743fcd55542db04c30663b9f86
Tagliapietra96 <tagliatek96@gmail.com>
2023-03-07 11:24:20 +0100

    fixed api routers

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\API\CategoryController;
+use App\Http\Controllers\API\DishController;
 use App\Http\Controllers\API\RestaurantController;
-use App\Models\Category;
-use App\Models\Dish;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,13 +21,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/restaurants', [Restaurant::class], 'index');
-Route::get('/restaurants/{restaurant}',[Restaurant::class, 'show']);
+Route::get('/restaurants', [RestaurantController::class, 'index']);
+Route::get('/restaurants/{restaurant}',[RestaurantController::class, 'show']);
 
-Route::get('/dishes', [Dish::class], 'index');
-Route::get('/dishes/{dish}',[Dish::class, 'show']);
+Route::get('/dishes', [DishController::class, 'index']);
+Route::get('/dishes/{dish}',[DishController::class, 'show']);
 
-Route::get('/categories', [Category::class], 'index');
-Route::get('/categories/{category}',[Category::class, 'show']);
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories/{category}',[CategoryController::class, 'show']);
 
 Route::get('/restaurants/search', [RestaurantController::class, 'search'])->name('restaurants.search');
