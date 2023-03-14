7ef053fdce5d223a46991dcfe74216b9332add59
VenturelliGiorgia <venturelli.giorgia.03@gmail.com>
2023-03-14 15:24:49 +0100

    create table

--- resources/views/restaurants/showOrders.blade.php
+++ resources/views/restaurants/showOrders.blade.php
@@ -1,9 +1,58 @@
-Ordini ricevuti
-@foreach ($orders as $order )
-    <div>{{$order->customer_name}}</div>
-    <div>{{$order->customer_email}}</div>
-    <div>{{$order->customer_address}}</div>
-    <div>{{$order->customer_phone}}</div>
-    <div>{{$order->created_at}}</div>
-    <div>{{$order->total_order}}€</div>
-@endforeach
+@extends('layouts.app')
+
+@section('content')
+    <div class="bg-dashboard pt-5">
+
+        <div class="container py-5 h-100 ">
+            <div class="container-form rounded-3">
+                <h1 class="fw-bolder custom-color">Ordini ricevuti</h1>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Cliente</th>
+                            <th scope="col">Indirizzo</th>
+                            <th scope="col">Numero di telefono</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Totale ordine</th>
+                            <th scope="col">Data</th>
+                            <th scope="col">Dettagli ordine</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        @foreach ($orders as $order )
+                            <td>
+                                <h6 class="py-3">{{$order->customer_name}}</h6>
+                            </td>
+                            <td>
+                                <h6 class="py-3">{{$order->customer_address}}</h6>
+                            </td>
+                            <td>
+                                <h6 class="py-3">{{$order->customer_phone}}</h6>
+                            </td>
+                            <td>
+                                <h6 class="py-3">{{$order->customer_email}}</h6>
+                            </td>    
+                            <td>
+                                <h6 class="py-3">{{ number_format($order->total_order, 2, ',', '.') }} €</h6>
+                            </td>
+                            <td>
+                                <h6 class="py-3">{{$order->created_at}}</h6>
+                            </td>    
+                            <td>
+                                <button class="btn btn-custom mt-2">Vedi dettagli</button>
+                            </td>
+                        </tr>
+                            @endforeach
+                            
+                    </tbody>
+                </table> 
+
+            </div>
+
+        </div>
+    </div>
+
+<script>
+</script>
+@endsection
