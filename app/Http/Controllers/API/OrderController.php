bbf7dccbc3c0bea667b9aed09fb6ac6b6cdb2506
PaoloWD <paolo.lore.93@gmail.com>
2023-03-14 19:29:17 +0100

    delete eseless code

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -60,13 +60,6 @@ class OrderController extends Controller
 
             $data = $request->validated();
             $order = Order::create($data);
-            if ($request->has("dish_id")) {
-                $order->dishes()->attach($data['dish_id']);
-                /* foreach ($data["dishesIds"] as $dishId) {
-                    $order->dishes()->attach($dishId, ['order_id' => $order->id]);
-                } */
-            }
-
             $order->save(); 
 
             Mail::to($order->customer_email)->send(new NewContactConfirmed($data));
