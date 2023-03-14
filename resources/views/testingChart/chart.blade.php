74753fa126a40100d65f192666e54316029bfad6
PaoloWD <paolo.lore.93@gmail.com>
2023-03-14 20:32:49 +0100

    complete statistic view

--- resources/views/testingChart/chart.blade.php
+++ resources/views/testingChart/chart.blade.php
@@ -4,132 +4,48 @@
 
     <div class="container py-5">
         <h4 class="pt-5 pb-3">Statistiche dei tuoi ordini:</h4>
-
-        <p>
-            possibili query in OrdersController
-            
-            $totalOrders = DB::table('orders')
-            ->where('restaurant_id', 1)
-            ->count();
-
-            $jenOrders= DB::table('orders')
-            ->whereMonth('created_at', '=', 1)
-            ->count();
-
-            $yearlyOrders = DB::table('orders')
-                    ->select(DB::raw('YEAR(created_at) as year'), DB::raw('count(*) as total'))
-                    ->groupBy('year')
-                    ->get();
-
-            foreach ($yearlyOrders as $year) {
-                echo 'Anno: ' . $year->year . ', Ordini totali: ' . $year->total;
-            }
-        </p>
-
-        <h6>scegliere grafico e divisione tabelle</h6>
-        
-        <canvas id="prova"></canvas>
-
-        <canvas id="orders" class="my-5"></canvas>
-
-        <canvas id="orders2" class="my-5"></canvas>
-        
-       
-
+        <canvas id="myChart"></canvas>
     </div>
-
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
     <script>
-        const canvas= document.getElementById('prova');
-        const orders= document.getElementById('orders');
-        const orders2= document.getElementById('orders2');
-
-        new Chart(canvas, {
-            type: 'line',
+        var chartData = {!! json_encode($chartData) !!};
+        var months = [];
+        var counts = [];
+        var sums = [];      
+        chartData.forEach(function(item){
+            months.push(item.month);
+            counts.push(item.count);
+            sums.push(item.sum);
+        });      
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
             data: {
-                labels: ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio'],
-                datasets: [
-                     {
-                     label: 'Totale Ordini',
-                     backgroundColor: '#f87979',
-                     data: [40, 39, 10, 40, 39, 80, 40]
-                     },
-                    {
-                    label: 'Totale incasso',
-                    backgroundColor: 'rgb(4, 91, 98)',
-                    data: [30, 20, 30, 10, 9, 4, 10],
-                    }
-                ]
-            },
-            
-            option: {
-                responsive: true,
-                maintainAspectRatio: true
-            }
-        })
-
-        new Chart(orders, {
-            type: 'line',
-            data: {
-                labels: ['Lasagne', 'Carbonara', 'Ragù', 'Costata', 'Maggio', 'Giugno', 'Luglio'],
-                datasets: [
-                    {
-                    label: 'Totale Ordini',
-                    backgroundColor: '#f87979',
-                    data: [40, 39, 10, 40, 39, 80, 40]
-                    },
-                ]
-            },
-            
-            option: {
-                responsive: true,
-                maintainAspectRatio: true
-            }
-        })
-
-        new Chart(orders2, {
-            type: 'radar',
-            data: {
-                labels: [
-                    'Eating',
-                    'Drinking',
-                    'Sleeping',
-                    'Designing',
-                    'Coding',
-                    'Cycling',
-                    'Running'
-                ],
+                labels: months,
                 datasets: [{
-                    label: 'My First Dataset',
-                    data: [65, 59, 90, 81, 56, 55, 40],
-                    fill: true,
+                    label: 'Conteggio ordini',
+                    data: counts,
                     backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                    borderColor: 'rgb(255, 99, 132)',
-                    pointBackgroundColor: 'rgb(255, 99, 132)',
-                    pointBorderColor: '#fff',
-                    pointHoverBackgroundColor: '#fff',
-                    pointHoverBorderColor: 'rgb(255, 99, 132)'
-                }, {
-                    label: 'My Second Dataset',
-                    data: [28, 48, 40, 19, 96, 27, 100],
-                    fill: true,
+                    borderColor: 'rgba(255, 99, 132, 1)',
+                    borderWidth: 1
+                },
+                {
+                    label: 'Incasso',
+                    data: sums,
                     backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                    borderColor: 'rgb(54, 162, 235)',
-                    pointBackgroundColor: 'rgb(54, 162, 235)',
-                    pointBorderColor: '#fff',
-                    pointHoverBackgroundColor: '#fff',
-                    pointHoverBorderColor: 'rgb(54, 162, 235)'
+                    borderColor: 'rgba(54, 162, 235, 1)',
+                    borderWidth: 1
                 }]
             },
             options: {
-                responsive: true,
-                maintainAspectRatio: true
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
             }
-        })
-
-
-        
-    </script>
-
+        });
+        </script>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DishController;
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RestaurantController;
 use Illuminate\Support\Facades\Auth;
@@ -37,12 +38,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/chart', [OrderController::class, 'chartDataByMonth'])->name('chart');
 });
 
 Route::get('/search', [RestaurantController::class, 'search'])->name('dishes.search'); 
 
-Route::get('/chart', function(){
+/* Route::get('/chart', function(){
     return view('testingChart.chart');
-});
+}); */
 
 require __DIR__.'/auth.php';
