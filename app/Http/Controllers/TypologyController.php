2c4713d9253016dba3b2f38e7b80f7c926829ec1
Pelliccia96 <fra.pelliccia96@gmail.com>
2023-03-17 17:14:56 +0100

    TypologyController Work in Progress

--- app/Http/Controllers/TypologyController.php
+++ app/Http/Controllers/TypologyController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Dish;
 use Illuminate\Http\Request;
 
 class TypologyController extends Controller
@@ -13,7 +14,9 @@ class TypologyController extends Controller
      */
     public function index()
     {
-        //
+        $types = Dish::select('type')->get();
+
+        return view('admin.typologies', compact('types'));
     }
 
     /**
