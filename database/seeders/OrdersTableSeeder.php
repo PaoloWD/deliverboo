d702cc7cfcd591de6d98eb10f8f2a52f3c09b797
PaoloWD <paolo.lore.93@gmail.com>
2023-03-18 10:39:28 +0100

    add ordersSeeder

--- database/seeders/OrdersTableSeeder.php
+++ database/seeders/OrdersTableSeeder.php
@@ -19,126 +19,136 @@ class OrdersTableSeeder extends Seeder
         // Ristorante da Paolo -> id: 2
         // Piatti Ristorante da Polo -> id: da 6 a 10
 
-        $orders = [
-            [
-                $dish_id = Dish::find(6)->id, // Recupera l'ID del piatto con ID = 6-10
-                $order = new Order(),
-                $order->status = 'In lavorazione',
-                $order->total_order = '8.00',
-                $order->order_time = '2023-01-17',
-                $order->order_date = '2023-01-17',
-                $order->customer_name = 'Mario Rossi',
-                $order->customer_address = 'Via Boolean Careers, 6',
-                $order->customer_phone = '3313131313',
-                $order->customer_email = 'mariorossi@gmail.com',
-                $order->restaurant_id = '2',
-                $order->created_at = '2023-01-16 09:01:46',
-                $order->updated_at = '2023-01-16 09:01:46',
-                $order->save(),
+        $dishIds = Dish::whereIn('id', [6, 7])->pluck('id')->toArray();
+        $dishIds2 = Dish::whereIn('id', [9, 7])->pluck('id')->toArray();
+        $dishIds3 = Dish::whereIn('id', [9, 7])->pluck('id')->toArray();
+        $dishIds4 = Dish::whereIn('id', [9, 7, 6, 8])->pluck('id')->toArray();
 
-                $order->dishes()->attach($dish_id), // Assegna l'ID dell'ordine alla tabella ponte
+        // Crea tre ordini associati a questi piatti
+        Order::create([
+            'status' => 'In lavorazione',
+            'total_order' => '18.50',
+            'order_time' => '2023/01/17',
+            'order_date' => '2023/01/17',
+            'customer_name' => 'Mario Rossi',
+            'customer_address' => 'Via Boolean Careers, 6',
+            'customer_phone' => '3313131313',
+            'customer_email' => 'mariorossi@gmail.com',
+            'restaurant_id' => '2',
+            'created_at' => '2023/01/16 09:01:46',
+            'updated_at' => '2023/01/16 09:01:46',
+        ])->dishes()->attach($dishIds);
 
-            ],
-            [
-                $dish_id = Dish::find(7)->id,
-                $order = new Order(),
-                $order->status = 'Completato',
-                $order->total_order = '10.50',
-                $order->order_time = '2023-02-17',
-                $order->order_date = '2023-02-17',
-                $order->customer_name = 'Giuseppe Verdi',
-                $order->customer_address = 'Via Boolean Careers, 7',
-                $order->customer_phone = '3311313131',
-                $order->customer_email = 'giuseppeverdi@gmail.com',
-                $order->restaurant_id = '2',
-                $order->created_at = '2023-02-17 09:01:47',
-                $order->updated_at = '2023-02-17 09:01:47',
-                $order->save(),
+        Order::create([
+            'status' => 'In lavorazione',
+            'total_order' => '27.00',
+            'order_time' => '2023/02/23',
+            'order_date' => '2023/02/23',
+            'customer_name' => 'Luigi Bianchi',
+            'customer_address' => 'Via False Street, 123',
+            'customer_phone' => '3355555555',
+            'customer_email' => 'luigibianchi@gmail.com',
+            'restaurant_id' => '2',
+            'created_at' => '2023/02/16 10:15:23',
+            'updated_at' => '2023/02/16 10:15:23',
+        ])->dishes()->attach($dishIds2);
 
-                $order->dishes()->attach($dish_id),
+        Order::create([
+            'status' => 'Completato',
+            'total_order' => '30.00',
+            'order_time' => '2023/03/23',
+            'order_date' => '2023/03/23',
+            'customer_name' => 'Giovanni Verdi',
+            'customer_address' => 'Via Boolean Careers, 8',
+            'customer_phone' => '3344444444',
+            'customer_email' => 'giovanniverdi@gmail.com',
+            'restaurant_id' => '2',
+            'created_at' => '2023/03/23 11:20:45',
+            'updated_at' => '2023/03/23 11:20:45',
+        ])->dishes()->attach($dishIds3);
 
-            ],
-/*             [
-                $dish_id = Dish::find()->id,
-                $order = new Order(),
-                $order->status = '',
-                $order->total_order = '',
-                $order->order_time = '',
-                $order->order_date = '',
-                $order->customer_name = '',
-                $order->customer_address = '',
-                $order->customer_phone = '',
-                $order->customer_email = '',
-                $order->restaurant_id = '2',
-                $order->created_at = '',
-                $order->updated_at = '',
-                $order->save(),
+        Order::create([
+            'status' => 'Completato',
+            'total_order' => '40.00',
+            'order_time' => '2023/02/23',
+            'order_date' => '2023/02/23',
+            'customer_name' => 'Giovanni Verdi',
+            'customer_address' => 'Via Boolean Careers, 8',
+            'customer_phone' => '3344444444',
+            'customer_email' => 'giovanniverdi@gmail.com',
+            'restaurant_id' => '2',
+            'created_at' => '2023/02/23 11:20:45',
+            'updated_at' => '2023/02/23 11:20:45',
+        ])->dishes()->attach($dishIds3);
 
-                $order->dishes()->attach($dish_id),
+        Order::create([
+            'status' => 'Completato',
+            'total_order' => '100.00',
+            'order_time' => '2023/03/23',
+            'order_date' => '2023/03/23',
+            'customer_name' => 'Giovanni Verdi',
+            'customer_address' => 'Via Boolean Careers, 8',
+            'customer_phone' => '3344444444',
+            'customer_email' => 'giovanniverdi@gmail.com',
+            'restaurant_id' => '2',
+            'created_at' => '2023/03/23 11:20:45',
+            'updated_at' => '2023/03/23 11:20:45',
+        ])->dishes()->attach($dishIds4);
 
-            ],
-            [
-                $dish_id = Dish::find()->id,
-                $order = new Order(),
-                $order->status = '',
-                $order->total_order = '',
-                $order->order_time = '',
-                $order->order_date = '',
-                $order->customer_name = '',
-                $order->customer_address = '',
-                $order->customer_phone = '',
-                $order->customer_email = '',
-                $order->restaurant_id = '2',
-                $order->created_at = '',
-                $order->updated_at = '',
-                $order->save(),
+        Order::create([
+            'status' => 'Completato',
+            'total_order' => '76.00',
+            'order_time' => '2023/03/23',
+            'order_date' => '2023/03/23',
+            'customer_name' => 'Paolo Meneguzzi',
+            'customer_address' => 'Via Boolean Careers, 8',
+            'customer_phone' => '3344444444',
+            'customer_email' => 'paolomeneguzzi@gmail.com',
+            'restaurant_id' => '2',
+            'created_at' => '2023/03/23 11:20:45',
+            'updated_at' => '2023/03/23 11:20:45',
+        ])->dishes()->attach($dishIds4);
 
-                $order->dishes()->attach($dish_id),
+        Order::create([
+            'status' => 'Completato',
+            'total_order' => '76.00',
+            'order_time' => '2023/03/23',
+            'order_date' => '2023/03/23',
+            'customer_name' => 'Paolo Meneguzzi',
+            'customer_address' => 'Via Boolean Careers, 8',
+            'customer_phone' => '3344444444',
+            'customer_email' => 'paolomeneguzzi@gmail.com',
+            'restaurant_id' => '2',
+            'created_at' => '2023/03/23 11:20:45',
+            'updated_at' => '2023/03/23 11:20:45',
+        ])->dishes()->attach($dishIds3);
 
-            ],
-            [
-                $dish_id = Dish::find()->id,
-                $order = new Order(),
-                $order->status = '',
-                $order->total_order = '',
-                $order->order_time = '',
-                $order->order_date = '',
-                $order->customer_name = '',
-                $order->customer_address = '',
-                $order->customer_phone = '',
-                $order->customer_email = '',
-                $order->restaurant_id = '2',
-                $order->created_at = '',
-                $order->updated_at = '',
-                $order->save(),
+        Order::create([
+            'status' => 'Completato',
+            'total_order' => '16.00',
+            'order_time' => '2023/02/23',
+            'order_date' => '2023/02/23',
+            'customer_name' => 'Paolo Meneguzzi',
+            'customer_address' => 'Via Boolean Careers, 8',
+            'customer_phone' => '3344444444',
+            'customer_email' => 'paolomeneguzzi@gmail.com',
+            'restaurant_id' => '2',
+            'created_at' => '2023/02/23 11:20:45',
+            'updated_at' => '2023/02/23 11:20:45',
+        ])->dishes()->attach($dishIds2);
 
-                $order->dishes()->attach($dish_id),
-
-            ],
-            [
-                $dish_id = Dish::find()->id,
-                $order = new Order(),
-                $order->status = '',
-                $order->total_order = '',
-                $order->order_time = '',
-                $order->order_date = '',
-                $order->customer_name = '',
-                $order->customer_address = '',
-                $order->customer_phone = '',
-                $order->customer_email = '',
-                $order->restaurant_id = '2',
-                $order->created_at = '',
-                $order->updated_at = '',
-                $order->save(),
-
-                $order->dishes()->attach($dish_id),
-
-            ], */
-        ];
-
-        // Ciclo sugli ordini e li inserisco nella tabella "orders"
-        foreach ($orders as $order) {
-            Order::create($order);
-        }
+        Order::create([
+            'status' => 'Completato',
+            'total_order' => '36.00',
+            'order_time' => '2023/02/23',
+            'order_date' => '2023/02/23',
+            'customer_name' => 'Paolo Meneguzzi',
+            'customer_address' => 'Via Boolean Careers, 8',
+            'customer_phone' => '3344444444',
+            'customer_email' => 'paolomeneguzzi@gmail.com',
+            'restaurant_id' => '2',
+            'created_at' => '2023/02/23 11:20:45',
+            'updated_at' => '2023/02/23 11:20:45',
+        ])->dishes()->attach($dishIds4);
     }
 }
